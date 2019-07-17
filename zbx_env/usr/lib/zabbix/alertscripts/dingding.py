951b6c3730b46d426ae9da8c44fad1c50ba5d251
guomaoqiu <guomaoqiu@sina.com>
2019-07-17 15:53:49 -0400

    first commit

--- zbx_env/usr/lib/zabbix/alertscripts/dingding.py
+++ zbx_env/usr/lib/zabbix/alertscripts/dingding.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding:utf-8 -*-
 # Author: guomaoqiu
 # QQ: 2399447849
 # Email: 2399447849@qq.com
@@ -9,6 +10,8 @@ import json
 import sys
 import os
  
+user = sys.argv[3]
+
 headers = {'Content-Type': 'application/json;charset=utf-8'}
 api_url = "https://oapi.dingtalk.com/robot/send?access_token=e832a6d764324fa5c961c4fce72319734fc614513be9ee00f0234c334b939372"
  
@@ -17,9 +20,9 @@ def msg(text):
      "msgtype": "text",
         "at": {
             "atMobiles": [
-               "13524894451"
+               user
             ],
-            "isAtAll": True
+            "isAtAll": False
         },
         "text": {
             "content": text
@@ -28,5 +31,5 @@ def msg(text):
     print requests.post(api_url,json.dumps(json_text),headers=headers).content
      
 if __name__ == '__main__':
-    text = sys.argv[1]
+    text = sys.argv[2] + "\n\n" + sys.argv[1]
     msg(text)
